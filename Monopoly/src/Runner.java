d5b2f36048ceec759dd62b451cca5adbe16daa4b
Grant <Paul@10.0.0.217>
2015-12-07 09:39:26 -0700

    Added other board

--- Monopoly/src/Runner.java
+++ Monopoly/src/Runner.java
@@ -55,13 +55,13 @@ public class Runner
 	
 	public static void addSquares()
 	{
-		System.out.println("Please select a board. Type 'Star Wars' for Star Wars and 'Vanillia' for original.");
+		System.out.println("Please select a board. Type '1' for Star Wars and '2' for original.");
 		
 		Scanner starA = new Scanner (System.in);
 		
-		String ban = starA.nextLine();
+		int ban = starA.nextInt();
 		
-		if (ban.equals("Star Wars"))
+		if (ban==1)
 			{
 		squares.add(new NoPurchase("Go","go",0));
 		squares.add(new Colored(false,false,"",60,0,0,"Purple",0,0,"Swamp - Dagobah",1));
@@ -105,10 +105,55 @@ public class Runner
 		squares.add(new Colored(false,false,"",400,0,0,"Purple",0,0,"Imperial Palace - Coruscant",39));
 			}
 		
-		if (ban.equals("Vanilla"))
+		else if (ban == 2)
 			{
-				
+			squares.add(new NoPurchase("Go","go",0));
+			squares.add(new Colored(false,false,"",60,0,0,"Brown",0,0,"Mediteranean Avenue",1));
+			squares.add(new NoPurchase("Community Chest", "drawCom",2));
+			squares.add(new Colored(false,false,"",60,0,0,"Brown",0,0,"Baltic Avenue",3));
+			squares.add(new NoPurchase("Income Tax", "tax", 4));
+			squares.add(new NoColor(false, false,"",200,0,0,"Reading Railroad",5));
+			squares.add(new Colored(false,false,"",100,0,0,"Light Blue",0,0,"Oriental Avenue",6));
+			squares.add(new NoPurchase("Chance", "drawCh", 7));
+			squares.add(new Colored(false,false,"",100,0,0,"Light Blue",0,0,"Vermont Avenue",8));
+			squares.add(new Colored(false,false,"",120,0,0,"Light Blue",0,0,"Connecticut Avenue",9));
+			squares.add(new NoPurchase("Just Visiting", "visit", 10));
+			squares.add(new Colored(false,false,"",140,0,0,"Light Pink",0,0,"St. Charles Place",11));
+			squares.add(new NoColor(false, false,"",150,0,0,"Electric Company",12));
+			squares.add(new Colored(false,false,"",140,0,0,"Light Pink",0,0,"States Avenue",13));
+			squares.add(new Colored(false,false,"",160,0,0,"Light Pink",0,0,"Virginia Avenue",14));
+			squares.add(new NoColor(false, false,"",200,0,0,"Pennsylvania Railroad",15));
+			squares.add(new Colored(false,false,"",180,0,0,"Orange",0,0,"St. James Place",16));
+			squares.add(new NoPurchase("Chance", "drawCh", 17));
+			squares.add(new Colored(false,false,"",180,0,0,"Orange",0,0,"Tennessee Avenue",18));
+			squares.add(new Colored(false,false,"",200,0,0,"Orange",0,0,"New York Avenue",19));
+			squares.add(new NoPurchase("Free Parking", "freePr", 20));
+			squares.add(new Colored(false,false,"",220,0,0,"Red",0,0,"Kentucky Avenue",21));
+			squares.add(new NoPurchase("Chance", "drawCh", 22));
+			squares.add(new Colored(false,false,"",220,0,0,"Red",0,0,"Indiana Avenue",23));
+			squares.add(new Colored(false,false,"",240,0,0,"Red",0,0,"Illinois Avenue",24));
+			squares.add(new NoColor(false, false,"",200,0,0,"B. & O. Railroad",25));
+			squares.add(new Colored(false,false,"",260,0,0,"Yellow",0,0,"Atlantic Avenue",26));
+			squares.add(new Colored(false,false,"",260,0,0,"Yellow",0,0,"Ventnor Avenue",27));
+			squares.add(new NoColor(false, false,"",150,0,0,"Water Works",28));
+			squares.add(new Colored(false,false,"",280,0,0,"Yellow",0,0,"Marvin Gardens",29));
+			squares.add(new NoPurchase("Go To Jail", "goJ", 30));
+			squares.add(new Colored(false,false,"",300,0,0,"Green",0,0,"Pacific Avenue",31));
+			squares.add(new Colored(false,false,"",300,0,0,"Green",0,0,"North Carolina Avenue",32));
+			squares.add(new NoPurchase("Community Chest", "drawCom", 33));
+			squares.add(new Colored(false,false,"",320,0,0,"Green",0,0,"Pennsylvania Avenue",34));
+			squares.add(new NoColor(false, false,"",200,0,0,"Short Line",35));
+			squares.add(new NoPurchase("Chance", "drawCh", 36));
+			squares.add(new Colored(false,false,"",350,0,0,"Purple",0,0,"Park Place",37));
+			squares.add(new NoPurchase("Luxury Tax", "taxL", 38));
+			squares.add(new Colored(false,false,"",400,0,0,"Purple",0,0,"Boardwalk",39));
 			}
+		
+		else 
+		{
+			System.out.println("Please choose a valid board.");
+			addSquares();
+		}
 	}
 	
 	public static void test()
